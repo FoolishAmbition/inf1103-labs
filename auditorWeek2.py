e536d53a5355acc6abc1c270597bbcc1e5b0f10d
ms <mingshun21@gmail.com>
2026-09-10 13:44:27 +0800

    added counter for the number of entrys

---
+++ auditorWeek2.py
@@ -0,0 +1,19 @@
+inventory = int(0)
+failed_entries = 0
+
+while True:
+   user_input = input("Enter the number of items of stock quantity please use integers (or type 'quit' to quit): ")
+   if user_input.lower() == 'quit':
+       print("Total Units Processed: ", inventory, "\n"
+           "Number of Failed/Rejected Entries.: ", failed_entries)
+       break
+   if user_input.isdigit():
+        inventory += int(user_input)
+   elif user_input.startswith('-') and user_input[1:].isdigit():
+        inventory += int(user_input)
+   else:
+        failed_entries += 1
+        print("Error: Please use integers only.")
+if inventory < 500:
+        print("Alert: Inventory is below 500.")
+
